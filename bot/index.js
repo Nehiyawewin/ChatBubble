a7cce08068ab2e663b67eb53495c176ec13fe580
Nehiyawewin <emihkwan@gmail.com>
2024-09-26 21:29:48 -0600

    new file:   bot/commands/chatbubble.js 	new file:   bot/deploycommands.js 	new file:   bot/events/ClientReady.js 	new file:   bot/events/InteractionCreate.js 	new file:   bot/index.js

---
+++ bot/index.js
@@ -0,0 +1,41 @@
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const path = require('node:path');
+const fs = require('node:fs');
+const { token } = require('../config.json');
+
+//https://discord.com/oauth2/authorize?client_id=1286535394199867472&permissions=294272493632&integration_type=0&scope=bot
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+client.cooldowns = new Collection();
+
+const commandFolder = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandFolder).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+
+	const filePath = path.join(commandFolder, file);
+
+	const command = require(filePath);
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+client.login(token);
